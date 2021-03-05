c72f72cde4919fc67cdfd7b4254bd85f52bddb1e
Jason Nordheim <jason.nordheim@gmail.com>
2021-03-05 10:03:35 -0700

    reduced index.js from template produced from create react app

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+function App() {
+  return <div>App Works</div>;
+}
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
