f64ddabb07aaeb2f24203acad212e62c04ab072a
Jason Nordheim <jason.nordheim@gmail.com>
2021-03-18 11:46:37 -0600

    replaced registration form on HomePage component for testing

--- src/components/pages/HomePage.js
+++ src/components/pages/HomePage.js
@@ -1,10 +1,11 @@
 import { Layout } from "../layout/Layout";
-import { RegisterForm } from "../forms/RegisterForm";
+//import { RegisterForm } from "../forms/RegisterForm";
+import { LoginForm } from "../forms/LoginForm";
 
 export function HomePage() {
   return (
     <Layout>
-      <RegisterForm />
+      <LoginForm />
     </Layout>
   );
 }
