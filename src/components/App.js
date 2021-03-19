4106f1900d2461521f5c7286347e2e36aa207b91
Jason Nordheim <jason.nordheim@gmail.com>
2021-03-19 11:43:28 -0600

    added basic styling through global stylesheet

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import { HomePage } from "./pages/HomePage";
 
+import "./App.css";
 export function App() {
   return (
     <BrowserRouter>
